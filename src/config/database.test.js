db8685d149d51ee769a4bdf460ad12e0973adc54
Emerson Andrey <emersonandrey_7@hotmail.com>
2019-02-11 11:58:06 -0300

    insert runTest DB

---
+++ src/config/database.test.js
@@ -0,0 +1,27 @@
+require('dotenv').load();
+const test = require('tape');
+const connectionDatabase = require('./database');
+
+function runTests(){
+    test('MongoDB Connection', tape => {
+        connectionDatabase.connect().then(connection => {
+            tape.assert(connection, "Connection established.");
+            tape.end()
+        }).catch(error => {
+            tape.assert(error, "Error connection DB.");
+            tape.end()
+        })
+    })
+ 
+    test('MongoDB Disconnection', tape => {
+        connectionDatabase.disconnect().then(disconnection => {
+            tape.assert(disconnection, "Disconnected.");
+            tape.end()
+        }).catch(error => {
+            tape.assert(error, "Error disconnected DB.");
+            tape.end()
+        })
+    })
+}
+
+runTests();
